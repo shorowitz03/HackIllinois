1f0239c5008761db18c6a450642bf685d7eaeed3
bvmble <alexluo92823@gmail.com>
2022-02-25 21:32:44 -0500

    git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,8 @@ import ContactUs from "./contactus";
 import Matches from "./Match";
 
 function App() {
-
   return (
     <Router>
-<<<<<<< HEAD
       <div className="App">
         <NavBar />
         <header className="App-header">
@@ -17,27 +15,13 @@ function App() {
             <Route exact path="/contactus">
               <ContactUs />
             </Route>
+
+            <Route exact path="/matches">
+              <Matches />
+            </Route>
           </Switch>
         </header>
       </div>
-=======
-    <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <Switch>
-          <Route exact path="/contactus">
-                <ContactUs />
-          </Route>
-
-          <Route exact path="/matches">
-                <Matches />
-          </Route>
-
-        </Switch>
-
-      </header>
-    </div>
->>>>>>> df5318c13a983a1e36bc9d2131bfcf872af7eb4e
     </Router>
   );
 }
