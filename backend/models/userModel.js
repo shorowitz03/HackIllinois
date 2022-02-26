22df60f4ffb91e3124df206bc5eb8e2a7646337f
bvmble <alexluo92823@gmail.com>
2022-02-26 12:47:46 -0500

    add user match route

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,10 +3,20 @@ require("dotenv").config();
 
 exports.make = (body) => {
   MongoClient.connect(process.env.CONNECTION_URI, async (err, client) => {
-    const db = client.db("HackIllinois2022");
-    await db.collection("users").insertOne(body)
-    // ... run functions on the collection
+    const db = client.db("userDB");
+    await db.collection("users").insertOne(body);
     client.close();
     callback(data);
   });
-}
+};
+
+exports.match = (query) => {
+  MongoClient.connect(process.env.CONNECTION_URI, async (err, client) => {
+    const db = client.db("userDB");
+    await db.collection("users").find({
+      // DEPENDS ON HOW DATA IS SENT + STORED
+    });
+    client.close();
+    callback(data);
+  });
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.get("/make", userController.make);
+router.post("/make", userController.make);
+
+router.get("/match", userController.match);
 
 module.exports = router;
