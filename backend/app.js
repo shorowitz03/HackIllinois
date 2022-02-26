2290705c307633929a9bdabbf795a3232e69943e
bvmble <alexluo92823@gmail.com>
2022-02-26 01:13:05 -0500

    add spotify login and auth routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth");
 const app = express();
 
-app.get("/api/test", (req, res) => {
-  res.send({ test: "hi" });
-});
+app.use("/api/auth", authRouter).use(cors()).use(cookieParser());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`)); // listen on proxy
