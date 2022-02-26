b5a18fe0ac7dd116470dc5e6506574fc6ce40ae9
garrett h <hurstg02@gmail.com>
2022-02-25 21:47:17 -0500

    started api pulling

---
+++ backend/connectScripts/yankData.py
@@ -0,0 +1,12 @@
+import spotipy
+import cred
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_ID, client_secret= cred.client_SECRET, redirect_uri=cred.redirect_url, scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
