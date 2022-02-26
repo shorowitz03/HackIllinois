c7baaf97ec772cf7044d360b8b9f6079a6d39c13
mmcdunn <mmcdunn@purdue.edu>
2022-02-25 21:10:51 -0500

    signup

---
+++ frontend/src/signup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class signup extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>sign up here!</div>
+        );
+    }
+}
+export default signup;
