cdafa5777af1d8dca398412aefae5b707ffc78dc
mmcdunn <mmcdunn@purdue.edu>
2022-02-25 20:02:31 -0500

    contactus

--- frontend/src/contactus.jsx
+++ frontend/src/contactus.jsx
@@ -1,3 +1,39 @@
 import React from "react";
 
-class contactus
+class contactus extends React.Component {
+    constructor(props) {
+        super(props)
+
+
+
+    }
+render () {
+    return (
+
+        <div>
+
+            wow this git stuff is insane
+
+
+
+        </div>
+
+
+
+
+
+    );
+
+
+
+
+
+
+}
+
+
+
+
+}
+
+export default contactus;
