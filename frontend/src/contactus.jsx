542b7502a1bdbc2c6ae62cc506f174aa6a67115c
mmcdunn <mmcdunn@purdue.edu>
2022-02-25 20:49:36 -0500

    contactus

--- frontend/src/contactus.jsx
+++ frontend/src/contactus.jsx
@@ -1,43 +1,24 @@
 import React from "react";
+//import Hero form '../components/Hero';
 
 class contactus extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            //name: ""
-            //email: ""
-            //message: ""
+            name: "",
+            email: "",
+            message: "",
+            disabled: false,
+            emailSent: null,
         }
         
-
-
-
-
     }
 render () {
     return (
-
-        <div>
-
-            wow this git stuff is insane ok
-
-
-
-        </div>
-
-
-
-
-
-    );
-
-
-
-
-
-
-}
-
+       /* <div>
+           <Hero title = (this.props.title) />
+        </div>/*
+      
 
 
 
